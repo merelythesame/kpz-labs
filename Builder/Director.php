5862971321fdaf65ca51a3cc1e368940a0a44b9e
ace <ipz232_pov@student.ztu.edu>
2025-03-18 21:42:27 +0000

    [FIX] Fixed fluent interface implementation

--- Builder/Director.php
+++ Builder/Director.php
@@ -6,23 +6,24 @@ class Director
 {
     public function makeHero(CharacterInterface $builder): void{
         $builder->reset();
-        $builder->setHeight(1.85);
-        $builder->setBodyType('Athletic');
-        $builder->setHairColor('Blonde');
-        $builder->setEyeColor('Blue');
-        $builder->setClothing(['Armor', 'Cape']);
-        $builder->setInventory(['Sword', 'Shield']);
-        $builder->setGoodDeeds(['Saved princess', 'Defeated a dragon']);
+        $builder->setHeight(1.85)
+            ->setBodyType('Athletic')
+            ->setBodyType('Athletic')
+            ->setHairColor('Blonde')
+            ->setEyeColor('Blue')
+            ->setClothing(['Armor', 'Cape'])
+            ->setInventory(['Sword', 'Shield'])
+            ->setGoodDeeds(['Saved princess', 'Defeated a dragon']);
     }
 
     public function makeEnemy(CharacterInterface $builder): void {
         $builder->reset();
-        $builder->setHeight(1.90);
-        $builder->setBodyType('Muscular');
-        $builder->setHairColor('Black');
-        $builder->setEyeColor('Red');
-        $builder->setClothing(['Dark Cloak', 'Spiked Armor']);
-        $builder->setInventory(['Poison Dagger', 'Dark Magic Tome']);
-        $builder->setBadDeeds(['Destroyed a village', 'Cursed a kingdom']);
+        $builder->setHeight(1.90)
+            ->setBodyType('Muscular')
+            ->setHairColor('Black')
+            ->setEyeColor('Red')
+            ->setClothing(['Dark Cloak', 'Spiked Armor'])
+            ->setInventory(['Poison Dagger', 'Dark Magic Tome'])
+            ->setBadDeeds(['Destroyed a village', 'Cursed a kingdom']);
     }
 }
